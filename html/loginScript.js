1d71f89439a35f0feb98026efce74dc4759aa19b
T. H <ceci_varna@abv.bg>
2018-11-16 11:57:11 +0100

    Add files via upload

---
+++ html/loginScript.js
@@ -0,0 +1,23 @@
+var from = document.getElementById('login_form');
+var username = document.getElementById('login_username');
+from.onsubmit = function(){
+    if(username.value.length >= 5){
+        sendUsername(username.value);
+    }
+    else{
+        username.classList.add("invalid");
+        prevUsername = username.value;
+    }
+    return false;
+}
+
+function sendUsername(username){
+
+}
+
+username.addEventListener("keyup", function(){
+    console.log("Change");
+    if(username.classList.contains("invalid") && username.value != prevUsername){
+        username.classList.remove("invalid");
+    }
+});
